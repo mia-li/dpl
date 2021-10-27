1a588ef1f62d03207b6af6329d223fde27d82153
mia-li <1007473330@qq.com>
2021-10-27 11:06:08 +0800

    多种布局切换stackwidget+splitter实现

---
+++ sscan.h
@@ -0,0 +1,16 @@
+#ifndef SSCAN_H
+#define SSCAN_H
+
+#include <QObject>
+
+class sscan : public QObject
+{
+    Q_OBJECT
+public:
+    explicit sscan(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // SSCAN_H
